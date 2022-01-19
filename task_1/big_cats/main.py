af658f292837e2c005e163d8dc1dbf9b01af4718
nielsRocholl <nielsrocholl@gmail.com>
2022-01-19 16:07:14 +0100

    big rewrite

--- task_1/big_cats/main.py
+++ task_1/big_cats/main.py
@@ -1,8 +1,8 @@
 #!/bin/python3
+import random
 
-
-from pipeline_modules.feature_extraction import FeatureExtractor
-from pipeline_modules.clustering import Clustering
+from pipeline_modules.feature_extraction import load_dataset
+from pipeline_modules.clustering import k_means, bag_of_words
 from sklearn import svm
 from sklearn.neighbors import KNeighborsClassifier
 import numpy as np
@@ -10,21 +10,31 @@ import numpy as np
 
 def main():
     # Classification pipeline:
-    f = FeatureExtractor()
-    c = Clustering(f.descriptor_list, f.num_of_samples, f.grey_dataset_flat_list, f.sift)
+    data = load_dataset()
+    # shuffle data
+    random.shuffle(data)
+    # split data into train and test
+    data_train = data[0: int(len(data) * .8)]
+    data_test = data[int(len(data) * .8):]
+    # use features to train knn
+    features = [i[0] for i in data_train]
+    kmeans = k_means(data=features, num_of_classes=5)
+    # calculate bag of words representation
+    bow = bag_of_words(kmeans=kmeans, data=data_train, num_of_classes=5)
 
-    x = [i[0] for i in c.bag_of_words]
-    y = [i[1] for i in c.bag_of_words]
-    print(np.shape(x), np.shape(y))
-    #svm
+    x = [i[0] for i in bow]  # histogram
+    y = [i[1] for i in bow]  # label
+    # svm
     clf = svm.NuSVC()
     clf.fit(x, y)
     # knn
     neigh = KNeighborsClassifier(n_neighbors=3)
     neigh.fit(x, y)
+    # perform testing
     acc_svm = 0
     acc_knn = 0
-    for hist, c in c.bag_of_words:
+    bow_test = bag_of_words(kmeans=kmeans, data=data_test, num_of_classes=5)
+    for hist, c in bow_test:
         pred_svm = clf.predict([hist])
         pred_knn = neigh.predict([hist])
         print(f'pred_svm: {pred_svm}, pred knn: {pred_knn}, real class: {c}')
@@ -32,9 +42,7 @@ def main():
             acc_svm += 1
         if pred_knn[0] == c:
             acc_knn += 1
-    print(f'final svm: {acc_svm/len(y)}, final_knn: {acc_knn/len(y)}')
-
-    # print(c.bag_of_words[10].sum())
+    print(f'final svm: {acc_svm / len(y)}, final_knn: {acc_knn / len(y)}')
 
 
 if __name__ == '__main__':
--- task_1/big_cats/pipeline_modules/clustering.py
+++ task_1/big_cats/pipeline_modules/clustering.py
@@ -1,69 +1,56 @@
+import os
 import numpy as np
 from sklearn.cluster import MiniBatchKMeans
 from joblib import dump, load
 from os.path import exists
 
 
-class Clustering:
-    def __init__(self, data, num_of_samples, grey_images, sift):
-        # paths:
-        self.__models_path = 'big_cats/models'
-
-        # variables:
-        self.num_of_classes = 5
-        self.data = data
-        self.grey_images = grey_images
-        self.num_of_samples = num_of_samples
-        self.k = self.num_of_classes * 10
-
-        # models:
-        self.kmeans = self.__kmeans()
-        self.sift = sift
-        self.bag_of_words = self.__hist()
-
-    def __kmeans(self):
-        """
-        Loads the kmeans model if it exists, else it creates the model
-        :return: kmeans model
-        """
-        if exists(f'{self.__models_path}/kmeans.joblib'):
-            print("Kmeans successfully loaded from file")
-            return load(f'{self.__models_path}/kmeans.joblib')
-        else:
-            batch_size = np.size(self.num_of_samples) * 3
-            print("Calculating kmeans. This might take a minute")
-            kmeans = MiniBatchKMeans(n_clusters=self.k, batch_size=batch_size, verbose=0).fit(self.data)
-
-            dump(kmeans, f'{self.__models_path}/kmeans.joblib')
-        print("Kmeans successfully created")
-        return kmeans
-
-    def __hist(self):
-        """
-        Loads the histogram list (bag of (visual) words) if it exists, else it creates the bag of words.
-        Every histogram should sum to 1, i.e. every entry represents the frequency of that particular descriptor
-        :return: list of histograms, (bag of words)
-        """
-        if exists(f'{self.__models_path}/bag_of_words.joblib'):
-            print("Histogram successfully loaded from file")
-            return load(f'{self.__models_path}/bag_of_words.joblib')
-        else:
-            self.kmeans.verbose = False
-            hist_list = []
-
-            for image, label in self.grey_images:
-                kp, des = self.sift.detectAndCompute(image, None)
-
-                hist = np.zeros(self.k)
-                nkp = np.size(kp)
-
-                for d in des:
-                    idx = self.kmeans.predict([d])
-                    hist[idx] += 1 / nkp  # Because we need normalized histograms, I prefere to add 1/nkp directly
-
-                hist_list.append([hist, label])
-            dump(hist_list, f'{self.__models_path}/bag_of_words.joblib')
-            print("Histogram successfully created")
-        return hist_list
+def k_means(data, num_of_classes):
+    """
+    Loads the kmeans model if it exists, else it creates the model
+    :return: kmeans model
+    """
+    descriptor_list = []
+    for features in data:
+        for d in features:
+            descriptor_list.append(d)
+    models_path = 'big_cats/models'
+    k = num_of_classes * 10
+    if exists(f'{models_path}/kmeans.joblib'):
+        print("Kmeans successfully loaded from file")
+        return load(f'{models_path}/kmeans.joblib')
+    else:
+        batch_size = np.size(len(data)) * 3
+        print("Calculating kmeans. This might take a minute")
+        kmeans = MiniBatchKMeans(n_clusters=k, batch_size=batch_size, verbose=0).fit(descriptor_list)
+        if not os.path.exists(f'{models_path}'):
+            # Create a new directory because it does not exist
+            os.makedirs(f'{models_path}')
+        dump(kmeans, f'{models_path}/kmeans.joblib')
+    print("Kmeans successfully created")
+    return kmeans
 
 
+def bag_of_words(kmeans, data, num_of_classes):
+    """
+    Loads the histogram list (bag of (visual) words) if it exists, else it creates the bag of words.
+    Every histogram should sum to 1, i.e. every entry represents the frequency of that particular descriptor
+    :return: list of histograms, (bag of words)
+    """
+    k = num_of_classes * 10
+
+    kmeans.verbose = False
+    hist_list = []
+
+    for des, kp, label in data:
+
+        hist = np.zeros(k)
+        nkp = np.size(kp)
+
+        for d in des:
+            idx = kmeans.predict([d])
+            hist[idx] += 1 / nkp  # Because we need normalized histograms, I prefere to add 1/nkp directly
+
+        hist_list.append([hist, label])
+    print("Histogram successfully created")
+    return hist_list
