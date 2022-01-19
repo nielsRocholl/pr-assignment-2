53238300342ef41278ef62791e54cfb4c45c363c
nielsRocholl <nielsrocholl@gmail.com>
2022-01-19 10:26:14 +0100

    svm + knn

--- task_1/big_cats/main.py
+++ task_1/big_cats/main.py
@@ -4,6 +4,7 @@
 from pipeline_modules.feature_extraction import FeatureExtractor
 from pipeline_modules.clustering import Clustering
 from sklearn import svm
+from sklearn.neighbors import KNeighborsClassifier
 import numpy as np
 
 
@@ -15,15 +16,23 @@ def main():
     x = [i[0] for i in c.bag_of_words]
     y = [i[1] for i in c.bag_of_words]
     print(np.shape(x), np.shape(y))
+    #svm
     clf = svm.NuSVC()
     clf.fit(x, y)
-    acc = 0
+    # knn
+    neigh = KNeighborsClassifier(n_neighbors=3)
+    neigh.fit(x, y)
+    acc_svm = 0
+    acc_knn = 0
     for hist, c in c.bag_of_words:
-        pred = clf.predict([hist])
-        print(f'pred: {pred}, real class: {c}')
-        if pred[0] == c:
-            acc += 1
-    print(f'final: {acc/len(y)}')
+        pred_svm = clf.predict([hist])
+        pred_knn = neigh.predict([hist])
+        print(f'pred_svm: {pred_svm}, pred knn: {pred_knn}, real class: {c}')
+        if pred_svm[0] == c:
+            acc_svm += 1
+        if pred_knn[0] == c:
+            acc_knn += 1
+    print(f'final svm: {acc_svm/len(y)}, final_knn: {acc_knn/len(y)}')
 
     # print(c.bag_of_words[10].sum())
 
