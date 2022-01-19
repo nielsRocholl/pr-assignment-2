6bf70cf2b64e24f32297bbafe3b95660f66f6ce1
nielsRocholl <nielsrocholl@gmail.com>
2022-01-19 16:50:17 +0100

    big rewrite

--- task_1/big_cats/main.py
+++ task_1/big_cats/main.py
@@ -1,11 +1,12 @@
 #!/bin/python3
 import random
+import numpy as np
 
 from pipeline_modules.feature_extraction import load_dataset
 from pipeline_modules.clustering import k_means, bag_of_words
 from sklearn import svm
 from sklearn.neighbors import KNeighborsClassifier
-import numpy as np
+from sklearn.model_selection import KFold
 
 
 def main():
@@ -13,36 +14,37 @@ def main():
     data = load_dataset()
     # shuffle data
     random.shuffle(data)
-    # split data into train and test
-    data_train = data[0: int(len(data) * .8)]
-    data_test = data[int(len(data) * .8):]
-    # use features to train knn
-    features = [i[0] for i in data_train]
-    kmeans = k_means(data=features, num_of_classes=5)
-    # calculate bag of words representation
-    bow = bag_of_words(kmeans=kmeans, data=data_train, num_of_classes=5)
-
-    x = [i[0] for i in bow]  # histogram
-    y = [i[1] for i in bow]  # label
-    # svm
-    clf = svm.NuSVC()
-    clf.fit(x, y)
-    # knn
-    neigh = KNeighborsClassifier(n_neighbors=3)
-    neigh.fit(x, y)
-    # perform testing
-    acc_svm = 0
-    acc_knn = 0
-    bow_test = bag_of_words(kmeans=kmeans, data=data_test, num_of_classes=5)
-    for hist, c in bow_test:
-        pred_svm = clf.predict([hist])
-        pred_knn = neigh.predict([hist])
-        print(f'pred_svm: {pred_svm}, pred knn: {pred_knn}, real class: {c}')
-        if pred_svm[0] == c:
-            acc_svm += 1
-        if pred_knn[0] == c:
-            acc_knn += 1
-    print(f'final svm: {acc_svm / len(y)}, final_knn: {acc_knn / len(y)}')
+    # create fold
+    kf = KFold(n_splits=5, random_state=None)
+    for fold, (train_index, test_index) in enumerate(kf.split(data)):
+        data_train, data_test = np.array(data, dtype=object)[train_index, :], np.array(data, dtype=object)[test_index, :]
+        # use features to train knn
+        features = [i[0] for i in data_train]
+        kmeans = k_means(data=features, num_of_classes=5)
+        # calculate bag of words representation
+        bow_train = bag_of_words(kmeans=kmeans, data=data_train, num_of_classes=5)
+        # retrieve x and y to train classifier
+        x = [i[0] for i in bow_train]  # histogram
+        y = [i[1] for i in bow_train]  # label
+        # svm
+        clf = svm.NuSVC()
+        clf.fit(x, y)
+        # knn
+        neigh = KNeighborsClassifier(n_neighbors=3)
+        neigh.fit(x, y)
+        # perform testing
+        acc_svm = 0
+        acc_knn = 0
+        bow_test = bag_of_words(kmeans=kmeans, data=data_test, num_of_classes=5)
+        for hist, c in bow_test:
+            pred_svm = clf.predict([hist])
+            pred_knn = neigh.predict([hist])
+            # print(f'pred_svm: {pred_svm}, pred knn: {pred_knn}, real class: {c}')
+            if pred_svm[0] == c:
+                acc_svm += 1
+            if pred_knn[0] == c:
+                acc_knn += 1
+        print(f'Fold: {fold}, final svm: {acc_svm / len(y)}, final_knn: {acc_knn / len(y)}')
 
 
 if __name__ == '__main__':
--- task_1/big_cats/pipeline_modules/clustering.py
+++ task_1/big_cats/pipeline_modules/clustering.py
@@ -1,8 +1,5 @@
-import os
 import numpy as np
 from sklearn.cluster import MiniBatchKMeans
-from joblib import dump, load
-from os.path import exists
 
 
 def k_means(data, num_of_classes):
@@ -14,19 +11,11 @@ def k_means(data, num_of_classes):
     for features in data:
         for d in features:
             descriptor_list.append(d)
-    models_path = 'big_cats/models'
+
     k = num_of_classes * 10
-    if exists(f'{models_path}/kmeans.joblib'):
-        print("Kmeans successfully loaded from file")
-        return load(f'{models_path}/kmeans.joblib')
-    else:
-        batch_size = np.size(len(data)) * 3
-        print("Calculating kmeans. This might take a minute")
-        kmeans = MiniBatchKMeans(n_clusters=k, batch_size=batch_size, verbose=0).fit(descriptor_list)
-        if not os.path.exists(f'{models_path}'):
-            # Create a new directory because it does not exist
-            os.makedirs(f'{models_path}')
-        dump(kmeans, f'{models_path}/kmeans.joblib')
+    batch_size = np.size(len(data)) * 3
+    print("Calculating kmeans. This might take a minute")
+    kmeans = MiniBatchKMeans(n_clusters=k, batch_size=batch_size, verbose=0).fit(descriptor_list)
     print("Kmeans successfully created")
     return kmeans
 
